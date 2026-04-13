560e1c37c603794fc10d1b49b70ddfbdb5b2539f
slothgina <gina.slothstudio@zohomail.com>
2026-04-13 19:35:55 -0400

    logical negation

--- boolean/TestProject/Program.cs
+++ boolean/TestProject/Program.cs
@@ -23,8 +23,15 @@
 //Console.WriteLine(1 >= 1);
 //Console.WriteLine(1 <= 1);
 
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(pangram.Contains("fox"));
+//Console.WriteLine(pangram.Contains("cow"));
+
+// These two lines of code will create the same output
 string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
+// These two lines of code will create the same output
+
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
 
 
--- boolean/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ boolean/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478ebfb673a3a298e2c76f5844a2b09104877542")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a9f9b154b6916f2929f7daf4a304c5cf6bda2dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
