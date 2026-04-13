478ebfb673a3a298e2c76f5844a2b09104877542
slothgina <gina.slothstudio@zohomail.com>
2026-04-13 19:30:32 -0400

    comparison operators

--- boolean/TestProject/Program.cs
+++ boolean/TestProject/Program.cs
@@ -11,10 +11,16 @@
 //string value2 = "A ";
 //Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
-Console.WriteLine("a" != "a");
-Console.WriteLine("a" != "A");
-Console.WriteLine(1 != 2);
+//Console.WriteLine("a" != "a");
+//Console.WriteLine("a" != "A");
+//Console.WriteLine(1 != 2);
 
-string myValue = "a";
-Console.WriteLine(myValue != "a");
+//string myValue = "a";
+//Console.WriteLine(myValue != "a");
+
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+
 
--- boolean/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ boolean/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e132a8189a9008db95650c27281e977738b7a1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587fb0293a4526c8ff2dbe4c297c2346f8f9cd33")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
