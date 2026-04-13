587fb0293a4526c8ff2dbe4c297c2346f8f9cd33
slothgina <gina.slothstudio@zohomail.com>
2026-04-13 19:28:50 -0400

    inequality operator

--- boolean/TestProject/Program.cs
+++ boolean/TestProject/Program.cs
@@ -7,7 +7,14 @@
 
 //Console.WriteLine("a" == "a ");
 
-string value1 = " a";
-string value2 = "A ";
-Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+//string value1 = " a";
+//string value2 = "A ";
+//Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a");
 
